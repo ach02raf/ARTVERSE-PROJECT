7624443e3a1a927e698b701aefd4dacecfbbb06e
mahagrindi <grindi.maha@gmail.com>
2023-04-06 01:59:06 +0100

    publiction affich

--- src/app/component/for-you-list/for-you-list.component.ts
+++ src/app/component/for-you-list/for-you-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { PublicationService } from '../../services/publication.service';
 
 @Component({
   selector: "app-for-you-list",
@@ -6,46 +7,43 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./for-you-list.component.scss"],
 })
 export class ForYouListComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private publicationService: PublicationService) {}
+    imageData: any;
+  List = [];
+  public commentText: string;
+  ngOnInit(): void {
+    this.getPubliction() 
+    }
 
-  List = [
-    {
-      _id: 1,
-      text: "ForYouListComponent",
-      date: "12/12/2022",
-      images: ["chester-wade.jpg"],
-      reaction: [{ likes: 1 }, { hate: 2 }],
-      commentaires: [{ iduser: 1, comment: "i like this image" }],
-      hashtag: ["#art", "#new"],
-    },
-    {
-      _id: 2,
-      text: "ForYouListComponent",
-      date: "13/12/2022",
-      images: ["mark-finn.jpg"],
-      reaction: [{ likes: 1 }, { hate: 2 }],
-      commentaires: [{ iduser: 1, comment: "i like this image" }],
-      hashtag: ["#art", "#new"],
-    },
-    // {
-    //   _id: 2,
-    //   text: "ForYouListComponent",
-    //   date: "13/12/2022",
-    //   images: ["lora.jpg"],
-    //   reaction: [{ likes: 1 }, { hate: 2 }],
-    //   commentaires: [{ iduser: 1, comment: "i like this image" }],
-    //   hashtag: ["#art", "#new"],
-    // },
-    // {
-    //   _id: 3,
-    //   text: "ForYouListComponent",
-    //   date: "15/12/2022",
-    //   images: ["img_3115.jpg"],
-    //   reaction: [{ likes: 1 }, { hate: 2 }],
-    //   commentaires: [{ iduser: 1, comment: "i like this image" }],
-    //   hashtag: ["#art", "#new"],
-    // },
-  ];
 
-  ngOnInit(): void {}
+  getPubliction() {
+    this.publicationService.getPost().subscribe((data) => {
+      this.List = data;
+      console.log(this.List);
+    });
+    
+  }
+
+  getImageUrl(id: any) {
+    this.publicationService.getImage({ id }).subscribe((data) => {
+      const imageUrl = "./../../../../../Dmwm-2-Backend/uploads/" + data;
+      return imageUrl;
+    });
+  }
+  
+  likePost(id: number) {
+    
+    const  data = {publicationId : id   , UserId : 2 };
+    this.publicationService.Reaction(data).subscribe((data) => {
+  alert(id);
+        });
+
+  }
+
+  // Dislike a post
+  dislikePost(id: number) {
+    
+    alert(id);
+  }
 }
--- src/app/services/publication.service.ts
+++ src/app/services/publication.service.ts
@@ -8,6 +8,9 @@ export class PublicationService {
 
   apiUrlGetHashtag = 'http://localhost:5000/Posts/GetTag';
   apiUrlCreatPost='http://localhost:5000/Posts/PostPublication';
+  apiUrlGetPost='http://localhost:5000/Posts/getPublications';
+  apiUrlGetOneImg='http://localhost:5000/Posts/getImage';
+  apiUrReactionPost='http://localhost:5000/Posts/reaction';
   constructor(private http: HttpClient) { }
 
   getMyHashtag(): Observable<any> {
@@ -17,4 +20,19 @@ export class PublicationService {
   createPost(postData: any): Observable<any> {
     return this.http.post(this.apiUrlCreatPost , postData);
   }
+
+  getPost(): Observable<any> {
+    return this.http.get(this.apiUrlGetPost );
+  }
+ 
+
+  getImage(id: any) { 
+    return this.http.get(`${this.apiUrlGetOneImg}?id=${id}`);
+  }
+  
+
+  Reaction(data: any): Observable<any> {
+    return this.http.put(this.apiUrReactionPost , data);
+  }
+
 }
