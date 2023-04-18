8085446ca914b4efef4d5c8dc689eb14b4d75105
ach02raf <ach02raf@gmail.com>
2023-04-18 01:14:32 +0100

    4-18-2023 1:14 h

--- src/app/component/for-you-list/for-you-list.component.ts
+++ src/app/component/for-you-list/for-you-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { PublicationService } from '../../services/publication.service';
+import { PublicationService } from "../../services/publication.service";
 import { LoggedInUserService } from "src/app/services/logged-in-user.service";
-
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import * as buffer from "buffer";
 @Component({
   selector: "app-for-you-list",
   templateUrl: "./for-you-list.component.html",
@@ -9,69 +10,89 @@ import { LoggedInUserService } from "src/app/services/logged-in-user.service";
 })
 export class ForYouListComponent implements OnInit {
   constructor(
-    private publicationService: PublicationService , private loggedUserServ: LoggedInUserService) {
-      this.idUser = this.loggedUserServ.getUserID();
-    }
-    imageData: any;
+    private publicationService: PublicationService,
+    private loggedUserServ: LoggedInUserService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.idUser = this.loggedUserServ.getUserID();
+  }
+  imageData: any;
   List = [];
   public commentText: string;
   loggedInUser: any;
   isCollapsed = true;
-
+  Listimage = [];
   idUser: any;
 
   ngOnInit(): void {
     this.loggedUserServ.findUserById(this.idUser).subscribe((res) => {
       this.loggedInUser = res;
-      console.log("foryoulist", this.loggedInUser);
+      // console.log("foryoulist", this.loggedInUser);
     });
-    this.getPubliction() ;
-     
-     
+    this.getPubliction();
+  }
+
+  async getPubliction() {
+    this.publicationService.getPost().subscribe(async (data) => {
+      this.List = await data;
+      // console.log("for you list post : ", this.List);
+      for (let item of this.List) {
+        let imageforpub = [];
+        for (let itam of item.img) {
+          // console.log("image :", itam);
+          this.publicationService
+            .getImage(itam.idimg)
+            .subscribe(async (data) => {
+              // console.log("image itam  : ", data);
+              const imageDataUrl = buffer.Buffer.from(
+                data["img"]["data"]["data"]
+              ).toString("base64");
+              const safeUrl: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(
+                `data:data:image/png;base64,${imageDataUrl}`
+              );
+              imageforpub.push({ _id: data["_id"], safeUrl: safeUrl });
+            });
+        }
+        // console.log("imageforpub : ", imageforpub);
+        this.Listimage.push({ idpub: item._id, listimage: imageforpub });
+      }
+      // console.log("listimagefor toute pub : ", this.Listimage);
+    });
+  }
+
+  getImage(idimage: any, idpub: any) {
+    for (let item of this.Listimage) {
+      if (item.idpub === idpub) {
+        for (let itam of item.listimage) {
+          if (itam._id === idimage) {
+            return itam.safeUrl;
+          }
+        }
+      }
     }
-
-
-  getPubliction() {
-    this.publicationService.getPost().subscribe((data) => {
-      this.List = data;
-      console.log(this.List);
-    });
-    
   }
 
-  getImageUrl(id: any) {
-    this.publicationService.getImage({ id }).subscribe((data) => {
-      const imageUrl = "./../../../../../Dmwm-2-Backend/uploads/" + data;
-      return imageUrl;
-    });
-  }
-  
   likePost(id: number) {
-    
-    const  data = {publicationId : id   , UserId : 0 };
+    const data = { publicationId: id, UserId: 0 };
     this.publicationService.Reaction(data).subscribe((data) => {
-  alert(id);
-        });
-
+      alert(id);
+    });
   }
 
   // Dislike a post
   reaction(list: any) {
-    
-   
-  const reactionIndex =list.findIndex(reaction => reaction.idUser === 0);
+    const reactionIndex = list.findIndex((reaction) => reaction.idUser === 0);
 
-  if (reactionIndex > -1) {
-    return true ;
-  } else {
-    return false ;
+    if (reactionIndex > -1) {
+      return true;
+    } else {
+      return false;
+    }
   }
+  reactioncount(list: any) {
+    return list.length;
   }
-  reactioncount(list : any ){
-    return list.length ;
+  achraf() {
+    return "hello";
   }
-
-
- 
-  
 }
