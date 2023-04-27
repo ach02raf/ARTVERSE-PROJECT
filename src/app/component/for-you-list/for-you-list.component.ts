13a82ce450c928019922f9c0f92ce26c63d62e0b
ach02raf <ach02raf@gmail.com>
2023-04-27 13:46:11 +0100

    valid

--- src/app/component/for-you-list/for-you-list.component.ts
+++ src/app/component/for-you-list/for-you-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { PublicationService } from "../../services/publication.service";
 import { LoggedInUserService } from "src/app/services/logged-in-user.service";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+//import { DatePipe } from '@angular/common';
+
 import * as buffer from "buffer";
 import { AuthentificationService } from "src/app/services/authentification.service";
 @Component({
@@ -11,6 +13,7 @@ import { AuthentificationService } from "src/app/services/authentification.servi
 })
 export class ForYouListComponent implements OnInit {
   constructor(
+    // private datePipe: DatePipe ,
     private publicationService: PublicationService,
     private loggedUserServ: LoggedInUserService,
     private sanitizer: DomSanitizer,
@@ -23,7 +26,7 @@ export class ForYouListComponent implements OnInit {
   ListCopy = [];
   public commentText: string;
   loggedInUser: any;
-  isCollapsed = true;
+  public isCollapsed: boolean[] = [];
   Listimage = [];
   idUser: any;
 
@@ -32,6 +35,7 @@ export class ForYouListComponent implements OnInit {
       this.loggedInUser = res;
       // console.log("foryoulist", this.loggedInUser);
     });
+
     this.getPubliction();
   }
 
@@ -44,7 +48,18 @@ export class ForYouListComponent implements OnInit {
   async getPubliction() {
     this.publicationService.getPost().subscribe(async (data) => {
       this.List = await data;
-      // console.log("for you list post : ", this.List);
+      this.List.forEach((item) => {
+        this.isCollapsed[item._id] = true;
+      });
+      console.log("for you list post : ", this.List);
+      this.List.forEach((item) => {
+        this.authserv.findUserById(item.Id_user).subscribe((data) => {
+          console.log("user winner", data);
+
+          this.ListCopy.push({ ...item, userData: data });
+          console.log("list with users", this.ListCopy);
+        });
+      });
       for (let item of this.List) {
         let imageforpub = [];
         for (let itam of item.img) {
@@ -82,7 +97,7 @@ export class ForYouListComponent implements OnInit {
   }
 
   likePost(id: number) {
-    const data = { publicationId: id, UserId: 0 };
+    const data = { publicationId: id, UserId: this.loggedInUser._id };
     this.publicationService.Reaction(data).subscribe((data) => {
       alert(id);
     });
@@ -103,5 +118,24 @@ export class ForYouListComponent implements OnInit {
   }
   achraf() {
     return "hello";
+  }
+
+  // to change the date  from 2023-04-18T00:29:38.643Z to 1h up to 23h and from 1week up to 10 week then in formt dd/mm/yyyy
+  format(date: string): string {
+    const now = new Date();
+    const diff = Math.abs(now.getTime() - Date.parse(date));
+    const diffHours = Math.floor(diff / (1000 * 60 * 60));
+    const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const diffWeeks = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
+
+    if (diffHours <= 23) {
+      return `${diffHours}h`;
+    } else if (diffDays <= 6) {
+      return `${diffDays}d`;
+    } else if (diffWeeks <= 10) {
+      return `${diffWeeks}w`;
+    } /*  else {
+      return this.datePipe.transform(Date.parse(date), 'dd/MM/yyyy');
+    } */
   }
 }
