b1f3a36c42a00171e791a9f661980ecc2e01db77
ach02raf <ach02raf@gmail.com>
2023-05-10 13:37:32 +0100

    5-10-2023 14h

--- src/app/component/show-challenge/show-challenge.component.ts
+++ src/app/component/show-challenge/show-challenge.component.ts
@@ -1,53 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { SinglesService } from "src/app/services/singles.service";
 import { AuthentificationService } from "src/app/services/authentification.service";
 
 @Component({
-  selector: 'app-show-challenge',
-  templateUrl: './show-challenge.component.html',
-  styleUrls: ['./show-challenge.component.scss']
+  selector: "app-show-challenge",
+  templateUrl: "./show-challenge.component.html",
+  styleUrls: ["./show-challenge.component.scss"],
 })
 export class ShowChallengeComponent implements OnInit {
-
-  challengesData : any ;
-  challengesCopy =[];
-  constructor(private singlesService : SinglesService ,     private authserv: AuthentificationService,
-    ) { }
+  challengesData: any;
+  challengesCopy = [];
+  constructor(
+    private singlesService: SinglesService,
+    private authserv: AuthentificationService
+  ) {}
 
   ngOnInit(): void {
     this.getchanlleng();
   }
 
-
-  getchanlleng(){
-    
-  
-    this.singlesService.get_chanllenge().subscribe(async (data) => { 
-         
-        this.challengesData = await data;
+  getchanlleng() {
+    this.singlesService.get_chanllenge().subscribe(async (data) => {
+      this.challengesData = await data;
 
       for (let item of this.challengesData) {
-
-        if (item['winner']){
-
-          this.authserv.findUserById(item['winner']).subscribe((data) => {
+        if (item["winner"]) {
+          this.authserv.findUserById(item["winner"]).subscribe((data) => {
             this.challengesCopy.push({ ...item, winnersData: data });
           });
-   
-          }
-        }}
-    );
-  } 
+        } else {
+          this.challengesCopy.push({ ...item });
+        }
+      }
+    });
+  }
 
   count(list: any) {
     return list.length;
   }
 
-  deteleChallenges(id : any , title : any ){
-this.singlesService.deleteChallnge({id : id}).subscribe((data)=>{
-  alert(title + "  has been deleted");
-
-});
+  deteleChallenges(id: any, title: any) {
+    this.singlesService.deleteChallnge({ id: id }).subscribe((data) => {
+      alert(title + "  has been deleted");
+    });
   }
-  
 }
